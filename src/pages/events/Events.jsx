bcacfa446e2012dc99e9223f537f9bff8e894a09
Anwesha.28 <sarkar.anwesha28@gmail.com>
2025-07-17 20:11:48 +0530

    Update Events.jsx

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -70,7 +70,7 @@ const Events = () => {
               onClick={() => setFilter(type)}
               className={`px-4 py-2 rounded transition ${
                 filter === type
-                  ? "bg-[#51B8F2] text-white"
+                  ? "bg-[#0B2044] text-white"
                   : "border border-[#0B2044] text-[#0B2044]"
               }`}
             >
