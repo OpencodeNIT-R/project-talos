a86963be70b651452f1ee2d1b9be82de8766e9af
Prasad <prasadpatra234@gmail.com>
2026-07-28 18:00:25 +0530

    bug fixed

--- src/pages/achievements/AchievementPage.jsx
+++ src/pages/achievements/AchievementPage.jsx
@@ -10,8 +10,6 @@ const AchievementPage = () => {
           className="absolute inset-0 bg-cover bg-no-repeat"
           style={{
             backgroundImage:
-              "url('https://res.cloudinary.com/ddoeahbpv/image/upload/v1782917393/IMG-20260118-WA0109_1_tdw08v.jpg')",
-            backgroundPosition:
               "url('https://res.cloudinary.com/dc7tu3hzv/image/upload/v1785234333/WhatsApp_Image_2026-06-29_at_18.22.43_mjqlt2.jpg')",
             // eslint-disable-next-line no-dupe-keys
             backgroundPosition: "center 90%",
